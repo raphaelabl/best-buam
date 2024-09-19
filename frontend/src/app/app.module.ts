9d0f3c258bd8e9db6488b2b0dcb08a49553805c8
raphaelabl <ablingerraphael@gmail.com>
2024-09-19 23:47:45 +0200

    mat update

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,7 +6,7 @@ import {AppComponent} from './app.component';
 import {DashboardComponent} from "./components/dashboard/dashboard.component";
 import {AdminComponent} from "./components/admin/admin.component";
 import {FormsModule} from "@angular/forms";
-import {HttpClientModule} from "@angular/common/http";
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 import {PartyAdminComponent} from './components/party-admin/party-admin.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {BuffetDialogComponent} from './components/_dialog/buffet-dialog/buffet-dialog.component';
@@ -14,25 +14,18 @@ import {WaiterComponent} from './components/waiter/waiter.component';
 import {BuffetComponent} from './components/buffet/buffet.component';
 import {MatDialogModule} from "@angular/material/dialog";
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    DashboardComponent,
-    AdminComponent,
-    PartyAdminComponent,
-    BuffetDialogComponent,
-    WaiterComponent,
-    BuffetComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    MatDialogModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        DashboardComponent,
+        AdminComponent,
+        PartyAdminComponent,
+        BuffetDialogComponent,
+        WaiterComponent,
+        BuffetComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        MatDialogModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
--- frontend/src/app/services/http.service.ts
+++ frontend/src/app/services/http.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import {Party} from "../models/party";
 import {environment} from "../../environments/environment";
 import {Observable} from "rxjs";
