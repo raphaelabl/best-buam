305c8f6dbcf6bec56025e290cdc40d9f58b9eda5
raphaelabl <ablingerraphael@gmail.com>
2025-05-13 22:54:15 +0200

    env

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
-  API_URL: "http://localhost:8080/",
+  API_URL: "https://order.ffbruckmuehl.at/auth/",
   WS_URL: "ws://localhost:8080/",
   keycloak: {
-    issuer: 'http://localhost:8081/auth/',
+    issuer: 'https://order.ffbruckmuehl.at/auth/',
     realm: 'bestbuam',
     clientId: 'bestbuam-frontend'
   }
